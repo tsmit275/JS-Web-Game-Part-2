cc295bc1fc2451ae379a0e12ee5ae74b0b935810
tsmit275 <taylor.lsmith03@gmail.com$ git config --global user.taylor.lsmith03@gmail.com>
2023-09-11 22:11:44 -0400

    final

--- index.js
+++ index.js
@@ -8,12 +8,17 @@ function newImage(url, left, bottom){
     return object
 }
 
-function newItem(url, left, bottom){
+function newItem(url, left, bottom) {
     let item = newImage(url, left, bottom)
+    item.addEventListener('click', function () {
+        item.remove()
+        let inventoryItem = document.createElement('img')
+        inventoryItem.src = url
+        inventory.append(inventoryItem)
+    })
 }
-
-function newInventory(){
-    let inventory = document.createElement('div')
+let inventory = document.createElement('div')
+function newInventory() {
     inventory.style.position = 'fixed'
     inventory.style.bottom = '0px';
     inventory.style.left = '0px'
